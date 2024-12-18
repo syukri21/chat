01b03dff942ec8ec33c0978efa063efd406ca50e
syukri21 <syukrihsb148@gmail.com>
2024-12-19 01:53:03 +0700

    Add `get_user` method to retrieve user by ID
    Introduce a new `get_user` method in `UserService` to fetch a user by their unique ID using an SQL query. Added corresponding unit test to validate the functionality, ensuring accurate retrieval of user details.

--- users/src/services/user_services.rs
+++ users/src/services/user_services.rs
@@ -1,7 +1,9 @@
 use crate::models::user::User;
+use chrono::NaiveDateTime;
 use persistence::DatabaseInterface;
 use std::sync::Arc;
-use chrono::NaiveDateTime;
+use sqlx::Row;
+use uuid::Uuid;
 
 pub struct UserService {
     db: Arc<dyn DatabaseInterface>,
@@ -12,29 +14,6 @@ impl UserService {
         Self { db }
     }
 
-    ///
-    ///
-    /// # Arguments
-    ///
-    /// Creates a new user in the database.
-    ///
-    /// # Arguments
-    ///
-    /// * `user` - A `User` struct containing the necessary details for the user to be created.
-    ///
-    /// # Returns
-    ///
-    /// * `anyhow::Result<()>` - Returns `Ok(())` if the user is successfully created, or an error if the operation fails.
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    ///
-    /// ```
-    /// * `user`:
-    ///
-    /// returns: Result<(), Error>
-    ///
     pub async fn create_user(&self, user: User) -> anyhow::Result<i64> {
         let mut connection = self.db.get_pool().acquire().await?;
 
@@ -53,9 +32,46 @@ impl UserService {
             .bind(user.updated_at.map(|dt| dt))
             .bind(None::<NaiveDateTime>)
             .execute(&mut *connection)
-            .await?.last_insert_rowid();
+            .await?
+            .last_insert_rowid();
 
         Ok(id)
+    }
+
+    pub async fn get_user(&self, id: Uuid) -> anyhow::Result<User> {
+        let mut connection = self.db.get_pool().acquire().await?;
+
+        let query = r#"SELECT
+            id,
+            username,
+            email,
+            password,
+            is_active,
+            pub_key,
+            created_at,
+            updated_at,
+            deleted_at
+        FROM users
+        WHERE id = ?"#;
+
+        let row = sqlx::query(query)
+            .bind(id.to_string())
+            .fetch_one(&mut *connection)
+            .await?;
+
+        let user = User {
+            id: row.try_get::<String, _>("id")?.parse()?,
+            username: row.try_get("username")?,
+            email: row.try_get("email")?,
+            password: row.try_get("password")?,
+            is_active: row.try_get("is_active")?,
+            pub_key: row.try_get("pub_key")?,
+            created_at: row.try_get("created_at")?,
+            updated_at: row.try_get("updated_at")?,
+            deleted_at: row.try_get("deleted_at").ok(),
+        };
+
+        Ok(user)
     }
 }
 #[cfg(test)]
@@ -80,7 +96,6 @@ mod tests {
         db
     }
 
-
     #[tokio::test]
     async fn test_create_user() {
         let db = setup().await;
@@ -97,7 +112,6 @@ mod tests {
         }
 
         {
-
             let user = User::new(
                 String::from("testnumber2"),
                 String::from("testnumber2@test.com"),
@@ -107,5 +121,38 @@ mod tests {
             let result = user_service.create_user(user).await;
             assert_eq!(result.unwrap(), 2);
         }
+    }
+    
+    #[tokio::test]
+    async fn test_get_user_by_userid() {
+        let db = setup().await;
+        let user_service = UserService::new(db);
+
+        // First, create a user to test retrieving it
+        let created_user = User::new(
+            String::from("test_get_user"),
+            String::from("test_get_user@test.com"),
+            String::from("testpassword"),
+            String::from("testpubkey"),
+        );
+        let result = user_service.create_user(created_user.clone()).await;
+        assert!(result.is_ok());
+
+        // Now, fetch the user by ID
+        let fetched_user = user_service.get_user(created_user.id).await.unwrap();
+
+        // Assert that fetched user matches the created user's details
+        assert_eq!(fetched_user.id, fetched_user.id);
+        assert_eq!(fetched_user.username, created_user.username);
+        assert_eq!(fetched_user.email, created_user.email);
+        assert_eq!(fetched_user.password, created_user.password);
+        assert_eq!(fetched_user.pub_key, created_user.pub_key);
+        assert_eq!(fetched_user.is_active, created_user.is_active);
+
+        // Check that created_at and updated_at are not None
+        assert!(fetched_user.created_at.is_some());
+        assert!(fetched_user.updated_at.is_some());
+        assert!(fetched_user.deleted_at.is_none());
+        
     }
 }
