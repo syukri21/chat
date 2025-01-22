21d46f83309d694678a5cc89dc154ded1fe0aea5
syukri21 <syukrihsb148@gmail.com>
2025-01-23 01:19:58 +0700

    refractor main web

--- crate/application/web/src/main.rs
+++ crate/application/web/src/main.rs
@@ -13,6 +13,23 @@ use tracing::{info_span, Span};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 #[tokio::main]
 async fn main() {
+    // initialize tracing
+    tracing_init();
+
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
+    let app = with_tracing(app);
+
+    // run it
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
+}
+
+fn tracing_init() {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
@@ -27,64 +44,55 @@ async fn main() {
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
+}
 
-    // build our application with a route
-    let app = Router::new()
-        .route("/", get(handler))
-        // `TraceLayer` is provided by tower-http so you have to add that as a dependency.
-        // It provides good defaults but is also very customizable.
-        //
-        // See https://docs.rs/tower-http/0.1.1/tower_http/trace/index.html for more details.
-        //
-        // If you want to customize the behavior using closures here is how.
-        .layer(
-            TraceLayer::new_for_http()
-                .make_span_with(|request: &Request<_>| {
-                    // Log the matched route's path (with placeholders not filled in).
-                    // Use request.uri() or OriginalUri if you want the real path.
-                    let matched_path = request
-                        .extensions()
-                        .get::<MatchedPath>()
-                        .map(MatchedPath::as_str);
+fn with_tracing(app: Router) -> Router {
+    // `TraceLayer` is provided by tower-http so you have to add that as a dependency.
+    // It provides good defaults but is also very customizable.
+    //
+    // See https://docs.rs/tower-http/0.1.1/tower_http/trace/index.html for more details.
+    //
+    // If you want to customize the behavior using closures here is how.
+    app.layer(
+        TraceLayer::new_for_http()
+            .make_span_with(|request: &Request<_>| {
+                // Log the matched route's path (with placeholders not filled in).
+                // Use request.uri() or OriginalUri if you want the real path.
+                let matched_path = request
+                    .extensions()
+                    .get::<MatchedPath>()
+                    .map(MatchedPath::as_str);
 
-                    info_span!(
-                        "http_request",
-                        method = ?request.method(),
-                        matched_path,
-                        trace_id = uuid::Uuid::new_v4().to_string().as_str(),
-                        some_other_field = tracing::field::Empty,
-                    )
-                })
-                .on_request(|_request: &Request<_>, _span: &Span| {
-                    // You can use `_span.record("some_other_field", value)` in one of these
-                    // closures to attach a value to the initially empty field in the info_span
-                    // created above.
-                })
-                .on_response(|_response: &Response, _latency: Duration, _span: &Span| {
-                    // ...
-                })
-                .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| {
-                    // ...
-                })
-                .on_eos(
-                    |_trailers: Option<&HeaderMap>, _stream_duration: Duration, _span: &Span| {
-                        // ...
-                    },
+                info_span!(
+                    "http_request",
+                    method = ?request.method(),
+                    matched_path,
+                    trace_id = uuid::Uuid::new_v4().to_string().as_str(),
+                    some_other_field = tracing::field::Empty,
                 )
-                .on_failure(
-                    |_error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {
-                        // ...
-                    },
-                ),
-        );
-
-    // run it
-    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
-    tracing::debug!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app)
-        .with_graceful_shutdown(shutdown_signal())
-        .await
-        .unwrap();
+            })
+            .on_request(|_request: &Request<_>, _span: &Span| {
+                // You can use `_span.record("some_other_field", value)` in one of these
+                // closures to attach a value to the initially empty field in the info_span
+                // created above.
+            })
+            .on_response(|_response: &Response, _latency: Duration, _span: &Span| {
+                // ...
+            })
+            .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| {
+                // ...
+            })
+            .on_eos(
+                |_trailers: Option<&HeaderMap>, _stream_duration: Duration, _span: &Span| {
+                    // ...
+                },
+            )
+            .on_failure(
+                |_error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {
+                    // ...
+                },
+            ),
+    )
 }
 
 async fn handler() -> Html<&'static str> {
